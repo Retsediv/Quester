b36038f64b88af648ec984d60b75ca8a351b9a12
retsediv <retsediv1@gmail.com>
2016-12-21 20:50:43 +0200

    Add contact page template

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 if __name__ == '__main__':
     app.run(debug=True)

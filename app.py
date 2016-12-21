5eb30a77cb3b688f7d0601dd856b3db99fdacf7c
retsediv <retsediv1@gmail.com>
2016-12-21 11:12:21 +0200

    Structure changing

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("parent.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)

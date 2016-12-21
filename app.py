1f26da36af4f37c89c7699b8bf588b922e17430e
retsediv <retsediv1@gmail.com>
2016-12-21 10:49:20 +0200

    Basic structure. Add materialecss

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("parent.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

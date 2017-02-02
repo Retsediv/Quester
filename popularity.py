c5d1a5289a1ca7176be5ecc7e4ea860d6b0b0a27
retsediv <retsediv1@gmail.com>
2017-02-02 22:40:11 +0200

    Make research based on tweets places

--- popularity.py
+++ popularity.py
@@ -1,6 +1,7 @@
 import sys
 import tweepy
 from tweepy import OAuthHandler
+from tweepy.streaming import json
 
 consumer_key = '73SF0RGxn2qJOUwPTbEAxAfJs'
 consumer_secret = 'bBfeyq01hodLQNe4uvymqaAeDMTGgKWaI1nTV49ReqlCFu208h'
@@ -15,5 +16,38 @@ api = tweepy.API(auth)
 # for place in api.geo_search(lat=49.8397, long=24.0297, accuracy=10000):
 # for place in tweepy.Cursor(api.search, count=1000, geocode="49.8397,24.0297,20km"):
 # for place in api.search(count=1000, geocode="49.8397,24.0297,20km"):
-#     # Process a single status
-#     print(place)
+
+# count = 0
+# with open("places.json", mode="a") as file:
+#     for place in tweepy.Cursor(api.search, count=100, geocode="49.8397,24.0297,20km", since_id=827018718790221824).items():
+#
+#         # Process a single status
+#         if place.coordinates is not None:
+#             count += 1
+#             print(count)
+#             file.write(json.dumps(place._json))
+#             file.write("\n")
+#
+# print(count)
+
+with open("places.json", 'r') as f:
+    geo_data = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for line in f:
+        tweet = json.loads(line)
+        if tweet['coordinates']:
+            geo_json_feature = {
+                "type": "Feature",
+                "geometry": tweet['coordinates'],
+                "properties": {
+                    "text": tweet['text'],
+                    "created_at": tweet['created_at']
+                }
+            }
+            geo_data['features'].append(geo_json_feature)
+
+# Save geo data
+with open('geo_data.json', 'w') as fout:
+    fout.write(json.dumps(geo_data, indent=4))
